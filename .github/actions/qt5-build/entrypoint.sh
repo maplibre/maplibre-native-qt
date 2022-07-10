c8808f3b9993b66d5798a5db3064448fa3a7be1f
Tadej Novak <tadej@tano.si>
2022-07-10 18:39:38 +0200

    Add Qt5 CI

---
+++ .github/actions/qt5-build/entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -l
+
+source scl_source enable devtoolset-8 rh-git218
+
+set -e
+set -x
+
+export CCACHE_DIR="$GITHUB_WORKSPACE/.ccache"
+export PATH=$Qt5_Dir/bin:$PATH
+qmake --version
+
+if [[ "$1" = "library" ]]; then
+  mkdir build && cd build
+  cmake ../source/dependencies/maplibre-gl-native/ \
+    -G Ninja \
+    -DMBGL_WITH_QT=ON \
+    -DMBGL_QT_LIBRARY_ONLY=ON \
+    -DMBGL_QT_STATIC=ON \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=../install-qmaplibregl \
+    -DCMAKE_C_COMPILER_LAUNCHER=ccache \
+    -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
+  ninja
+  ninja install
+elif [[ "$1" = "plugin" ]]; then
+  mkdir build && cd build
+  qmake ../source/ QMAPLIBREGL_PATH=../install-qmaplibregl
+  make -j2
+  INSTALL_ROOT=../install make install
+else
+  exit 1
+fi
