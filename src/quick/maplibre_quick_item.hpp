21275d2452c796fa081375a266a4e3a025a29366
Birk Skyum <birk.skyum@pm.me>
2025-07-06 22:21:54 +0200

    support opengl

--- src/quick/maplibre_quick_item.hpp
+++ src/quick/maplibre_quick_item.hpp
@@ -1,52 +1,13 @@
 #pragma once
 
-#include <CoreFoundation/CoreFoundation.h>
-#include <QtQml/qqmlregistration.h>
-#include <QMapLibre/Map>
-#include <QQuickItem>
-#include <memory>
+// MapLibre Quick Item - Main header that selects the appropriate backend implementation
 
-namespace QMapLibre {
-class Map;
-}
+#if defined(MLN_RENDER_BACKEND_OPENGL)
+#include "maplibre_quick_item_opengl.hpp"
+#elif defined(MLN_RENDER_BACKEND_VULKAN)
+#include "maplibre_quick_item_vulkan.hpp"
+#elif defined(MLN_RENDER_BACKEND_METAL)
+#include "maplibre_quick_item_metal.hpp"
+#endif
 
-namespace QMapLibreQuick {
-class MapLibreQuickItem : public QQuickItem {
-    Q_OBJECT
-    QML_NAMED_ELEMENT(MapLibreView)
-public:
-    MapLibreQuickItem();
-    ~MapLibreQuickItem() override {
-        if (m_currentDrawable) {
-            CFRelease(m_currentDrawable);
-        }
-    }
-
-protected:
-    QSGNode *updatePaintNode(QSGNode *, UpdatePaintNodeData *) override;
-    void geometryChange(const QRectF &newGeometry, const QRectF &oldGeometry) override;
-    void releaseResources() override;
-    void itemChange(ItemChange change, const ItemChangeData &data) override;
-
-    // Mouse interaction
-    void mousePressEvent(QMouseEvent *) override;
-    void mouseMoveEvent(QMouseEvent *) override;
-    void mouseReleaseEvent(QMouseEvent *) override;
-    void wheelEvent(QWheelEvent *) override;
-
-private:
-    void ensureMap(int w, int h, float dpr, void *metalLayer);
-
-    std::unique_ptr<QMapLibre::Map> m_map;
-    QSize m_size;
-    bool m_connected{false};
-    bool m_rendererBound = false;
-    void *m_layerPtr = nullptr;        // persistent CAMetalLayer pointer we pass to MapLibre
-    bool m_ownsLayer = false;          // true when we created the fallback CAMetalLayer
-    void *m_currentDrawable = nullptr; // retained CAMetalDrawable until frame ends
-
-    // interaction state
-    QPointF m_lastMousePos;
-    bool m_dragging{false};
-};
-} // namespace QMapLibreQuick
+// The MapLibreQuickItem type alias is defined in the specific backend headers
---
+++ src/quick/maplibre_quick_item_base.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <QtQml/qqmlregistration.h>
+#include <QMapLibre/Map>
+#include <QQuickItem>
+#include <QString>
+#include <memory>
+
+namespace QMapLibre {
+class Map;
+}
+
+namespace QMapLibreQuick {
+
+/**
+ * @brief Base class for MapLibre Quick item implementations
+ * 
+ * This class contains the common interface and functionality shared
+ * across all rendering backend implementations (OpenGL, Metal, Vulkan).
+ */
+class MapLibreQuickItemBase : public QQuickItem {
+    Q_OBJECT
+    
+public:
+    MapLibreQuickItemBase();
+    virtual ~MapLibreQuickItemBase() = default;
+
+protected:
+    // Common interface that all backends must implement
+    QSGNode *updatePaintNode(QSGNode *, UpdatePaintNodeData *) override;
+    void geometryChange(const QRectF &newGeometry, const QRectF &oldGeometry) override;
+    void releaseResources() override;
+    void itemChange(ItemChange change, const ItemChangeData &data) override;
+
+    // Mouse interaction - common to all backends
+    void mousePressEvent(QMouseEvent *) override;
+    void mouseMoveEvent(QMouseEvent *) override;
+    void mouseReleaseEvent(QMouseEvent *) override;
+    void wheelEvent(QWheelEvent *) override;
+
+    // Backend-specific virtual methods
+    virtual QSGNode* renderFrame(QSGNode* oldNode) = 0;
+    virtual void initializeBackend() = 0;
+    virtual void cleanupBackend() = 0;
+
+    // Common helper methods
+    void ensureMap(int w, int h, float dpr, void *backendSpecificData = nullptr);
+
+    // Common member variables
+    std::unique_ptr<QMapLibre::Map> m_map;
+    QSize m_size;
+    bool m_connected{false};
+    bool m_rendererBound = false;
+
+    // Interaction state
+    QPointF m_lastMousePos;
+    bool m_dragging{false};
+};
+
+} // namespace QMapLibreQuick
