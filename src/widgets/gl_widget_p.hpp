0f83f0650e465f9a625592fa43737b3aacd2dabf
Birk Skyum <birk.skyum@pm.me>
2025-07-26 17:26:43 +0200

    opengl widget run again

--- src/widgets/gl_widget_p.hpp
+++ src/widgets/gl_widget_p.hpp
@@ -14,6 +14,7 @@ QT_BEGIN_NAMESPACE
 class QKeyEvent;
 class QMouseEvent;
 class QWheelEvent;
+class QOpenGLShaderProgram;
 
 QT_END_NAMESPACE
 
@@ -32,6 +33,12 @@ public:
 
     std::unique_ptr<Map> m_map{};
     Settings m_settings;
+    
+    // Zero-copy texture rendering members
+    std::unique_ptr<QOpenGLShaderProgram> m_shaderProgram{};
+    unsigned int m_vertexBuffer{0};
+    unsigned int m_vao{0};
+    unsigned int m_mapFramebuffer{0};
 
 private:
     Q_DISABLE_COPY(GLWidgetPrivate);
