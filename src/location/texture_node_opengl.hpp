02c8d0dd00bb5346845b73235363495ff71b3a94
Birk Skyum <birk.skyum@pm.me>
2025-07-28 04:09:21 +0200

    support the location/qml  for opengl

--- src/location/texture_node_opengl.hpp
+++ src/location/texture_node_opengl.hpp
@@ -4,18 +4,22 @@
 #pragma once
 
 #include "texture_node_base.hpp"
+#include "qgeomap.hpp"
+#include <qopengl.h>
 
 namespace QMapLibre {
 
 class TextureNodeOpenGL : public TextureNodeBase {
 public:
     TextureNodeOpenGL(const Settings &settings, const QSize &size, qreal pixelRatio, QGeoMapMapLibre *geoMap);
+    ~TextureNodeOpenGL() override;
 
     void resize(const QSize &size, qreal pixelRatio, QQuickWindow *window) override;
     void render(QQuickWindow *window) override;
 
 private:
     bool m_rendererBound = false;
+    GLuint m_fbo = 0;
 };
 
 } // namespace QMapLibre
